fe4d8cb4f6ac6fbf88501e939cbe52591b11fca0
catrionameriel <catriona.meriel@gmail.com>
2018-01-21 11:38:34 +0000

    canAddToBasket test passed

--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -1,4 +1,5 @@
 import basket.Basket;
+import basket.Item;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,14 +8,22 @@ import static org.junit.Assert.assertEquals;
 public class BasketTest {
 
     private Basket basket;
+    private Item brie;
 
     @Before
     public void before() {
        basket = new Basket();
+       brie = new Item("Brie", 7.25);
     }
 
     @Test
     public void itemsInBasketStartsAtZero() {
         assertEquals(0, basket.countItemsInBasket());
+    }
+
+    @Test
+    public void canAddToBasket() {
+        basket.addItemsToBasket(brie);
+        assertEquals(1, basket.countItemsInBasket());
     }
 }
